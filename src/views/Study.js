23cd8a191d4633ed401faaad3ffe1bc008d76751
Adam Makinson <adam.levi.makinson@gmail.com>
2024-12-30 21:55:33 -0500

    write createWordsCollection function to split the words into a data structure that will allow splitting into phrases, displaying spoken words and difff highlights

--- src/views/Study.js
+++ src/views/Study.js
@@ -61,6 +61,46 @@ function a11yProps(index) {
   };
 }
 
+/**
+ * This function takes a string of scripture text and splits it on a space.
+ * It then loops through each word creating an object:
+ *      If the "word" ends with punctuation, increment the phrase counter and add phrase to the object
+ *      Add an index to the object
+ *      Initialize "said" and "correct" both to boolean false
+ *      add the word to a "word" property
+ * 
+ * @param {string} scriptureText
+ * 
+ * returns an array of word objects 
+ */
+function createWordsCollection(scriptureText) {
+    let scriptureSimpleWordArray = scriptureText.split(/\s+/);
+    let scriptureWordsCollection = [];
+    let currentWord;
+    let currentWordObject;
+    let currentPhrase = 1;
+
+    for (let i=0; i < scriptureSimpleWordArray.length; i++) {
+        currentWord = scriptureSimpleWordArray[i];
+        currentWordObject = {};
+
+        currentWordObject.idx = i;
+        currentWordObject.word = currentWord;
+        currentWordObject.said = false;
+        currentWordObject.correct = null;
+
+        currentWordObject.phrase = currentPhrase;
+        
+        if (currentWord.match(/[.,/#!$%^&*;:{}=\-_`~()]/gu)) {
+            currentPhrase++
+        }
+
+        scriptureWordsCollection.push(currentWordObject)
+    }
+
+    return scriptureWordsCollection;
+}
+
 export default function Study() {
   const {
     finalTranscript,
@@ -79,16 +119,7 @@ export default function Study() {
     scripture.text = v.content;
   }
 
-  scripture.replacedText = replaceText(scripture.text)
-  scripture.splitText = scripture.replacedText.split(/\s+/)
-  const [activeTab, setActiveTab] = useState(0)
-  const [spokenText, setSpokenText] = useState("")
-  const [currentWordIndex, setCurrentWordIndex] = useState(0)
-  const [testSubmission, setTestSubmission] = useState("")
-  const [testResult, setTestResult] = useState([])
-  const [allSpoken, setAllSpoken] = useState("")
-  const [scripturePhrases, setScripturePhrases] = useState([])
-  const [phraseIndex, setPhraseIndex] = useState(0)
+    let scriptureWordCollection = createWordsCollection(scripture.text);
 
   useEffect(() => {
 
